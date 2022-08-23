963ea7c07c40b012ae2a7b4b47441062e2f5b507
andressa-leite <dre_tl@hotmail.com>
2022-08-23 20:09:31 -0300

    Evento de input configurado em todos os botões

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -4,11 +4,12 @@
  function Boton(props){
 
     const esOperador = valor => {
-        return isNaN(valor) && (valor != '.') && (valor != '=')
+        return isNaN(valor) && (valor !== '.') && (valor !== '=')
     };
 
     return (
-        <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : null}`}>
+        <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()} 
+            onClick={() => props.manejarClic(props.children)}>
             {props.children}
         </div>
     )
